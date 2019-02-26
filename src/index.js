425ed100e1d82a426d524ad4ed75044cd10181b1
pjonone <pjon.one@gmail.com>
2019-02-26 15:56:46 +0300

    Test passed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var error = {
+        error: "You are rich, my friend! We don't have so much coins for exchange"
+    };
+    var cash = {};
+    if (currency > 10000)
+        return error;
+    else if (currency <= 0) return cash;
+    else {
+     var h = Math.floor(currency/50);
+     if (h > 0) cash.H = h;
+     var q = Math.floor((currency - 50*h)/25);
+     if (q > 0) cash.Q = q;
+     var d = Math.floor((currency - 50*h - 25*q)/10);
+     if (d > 0) cash.D = d;
+     var n = Math.floor((currency - 50*h - 25*q - 10*d)/5);
+     if (n > 0) cash.N = n;
+     var p = Math.floor((currency - 50*h - 25*q - 10*d - 5*n)/1);
+     if (p > 0) cash.P = p;
+
+
+        return cash;
+    }
+
 }
